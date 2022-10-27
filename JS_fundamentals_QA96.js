0dba21d075b1ec5a9cce2e3ccbeb88b1f6ed5507
natapage <624832@gmail.com>
2022-10-27 22:36:43 +0300

    feat: add for_while tasks

--- JS_fundamentals_QA96.js
+++ JS_fundamentals_QA96.js
@@ -186,3 +186,20 @@ if (y < 1) {
 } 
 
 console.log(pow(x,y));
+
+
+// Замените код Function Expression стрелочной функцией:
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask(
+  "Вы согласны?",
+  function() { alert("Вы согласились."); },
+  function() { alert("Вы отменили выполнение."); }
+);
+
+
+ask("Вы согласны?", () => alert("Вы согласились.", () => alert("Вы отменили выполнение.");
