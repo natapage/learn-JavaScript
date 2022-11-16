a8bfb77f287d8fdd20f04cff7d73995dc66c7a33
natapage <624832@gmail.com>
2022-11-16 11:34:51 +0300

    feat: learn JS tasks

--- JS_arrays_methods_QA102.js
+++ JS_arrays_methods_QA102.js
@@ -178,6 +178,53 @@ let sorted = copySorted(arr);
 alert( sorted ); // CSS, HTML, JavaScript
 alert( arr ); // HTML, JavaScript, CSS (без изменений)
 
-function copySorted(arr     ) {
-
+function copySorted(arr) {
+  return arr.slice().sort();
 }
+
+// _______________________________________________________________________________________________________________________
+
+// Создайте функцию конструктор Calculator, которая создаёт «расширяемые» объекты калькулятора.
+// Задание состоит из двух частей.
+// Во-первых, реализуйте метод calculate(str), который принимает строку типа "1 + 2"
+//  в формате «ЧИСЛО оператор ЧИСЛО» (разделено пробелами) и возвращает результат. Метод должен понимать плюс + и минус -.
+// Пример использования:
+
+let calc = new Calculator;
+
+alert( calc.calculate("3 + 7") ); // 10
+
+// Затем добавьте метод addMethod(name, func), который добавляет в калькулятор новые операции. 
+// Он принимает оператор name и функцию с двумя аргументами func(a,b), которая описывает его.
+// Например, давайте добавим умножение *, деление / и возведение в степень **:
+
+let powerCalc = new Calculator;
+powerCalc.addMethod("*", (a, b) => a * b);
+powerCalc.addMethod("/", (a, b) => a / b);
+powerCalc.addMethod("**", (a, b) => a ** b);
+
+let result = powerCalc.calculate("2 ** 3");
+alert( result ); // 8
+// Для этой задачи не нужны скобки или сложные выражения.
+// Числа и оператор разделены ровно одним пробелом.
+// Не лишним будет добавить обработку ошибок.
+
+function Calculator() {
+  this.calculate = function(str) {
+    const [a, operator, b] = str.split(' ');
+    return this[operator](+a, +b)
+  };
+  this.addMethod = function(name, func) {
+    this[name] = func;
+  };
+  this['+'] = (a, b) => a + b;
+  this['-'] = (a, b) => a - b;
+}
+
+const obj = new Calculator();
+obj.addMethod("*", (a, b) => a * b);
+obj.addMethod("/", (a, b) => a / b);
+obj.addMethod("**", (a, b) => a ** b);
+obj.calculate("3 * 7");
+obj.calculate("3 + 7")
+
