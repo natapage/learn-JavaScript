1d9a1d1e5dad31ec1b4148dfb742c0471adc9526
natapage <624832@gmail.com>
2023-03-18 21:53:33 +0300

    debts

--- debts_current.js
+++ debts_current.js
@@ -2,7 +2,6 @@
 
 // динамический импорт
 // реэкспорт
-// как запускается скрипт в head и в body (разница type ='module' с и без, как работает defer)
 // FSD (feature sliced design)
 // vue3 дока
 // курс по vue от ulbi
